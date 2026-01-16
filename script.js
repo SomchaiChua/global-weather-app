0bc7c6a1a96edbc74174182eb542eac5bc2e53cb
Somchai Chuananon <somchai.nbhospital@gmail.com>
2026-01-16 20:14:36 +0700

    Add timezone calculation function

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 console.log('Weather App Loaded');
 function getWeather(city) { console.log('Fetching data for ' + city); }
+function getTimeZone(offset) { console.log('Calculating time for offset: ' + offset); }
